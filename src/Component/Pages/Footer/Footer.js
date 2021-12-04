7bf15868a015bd6e7fa4cdecc0957e56a6758637
Nure-Alam-sidddique <74979607+Nure-Alam-sidddique@users.noreply.github.com>
2021-12-04 18:47:41 +0600

    emailjs Update

--- src/Component/Pages/Footer/Footer.js
+++ src/Component/Pages/Footer/Footer.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="h-36 bg-gray-900 relative text-center ">
+        <div className="h-auto mb-5 bg-gray-900 relative text-center ">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+                <div className="h-32 w-full bg-red-100"></div>
+                <div className="h-32 w-full bg-red-100"></div>
+                <div className="h-32 w-full bg-red-100"></div>
+            </div>
 
-            <hp className="h-200  text-white   absolute bottom-0">Developed By @ Nure Alam Siddique</hp>
+            <p className=" text-red-600   absolute bottom-0">Developed By @ Nure Alam Siddique</p>
         </div>
     );
 };
--- src/Component/Pages/Home/Contact/Contact.js
+++ src/Component/Pages/Home/Contact/Contact.js
@@ -1,14 +1,25 @@
 import emailjs from 'emailjs-com';
-import React from 'react';
+import React, { useState } from 'react';
 const Contact = () => {
+  const [result, setShowResult] = useState(false);
+
+  const showResult = () => {
+    return (<p className="text-xl text-green-50 text-center border p-3 mb-3 w-1/2 mx-auto">Your message successfully send!!!</p>);
+  }
+  // hide result
+  setTimeout(() => { setShowResult(false) }, 5000);
+  
+  //  Email Send function
     const sendEmail = e => {
         emailjs.sendForm(
-            "service_iyum74f",
+            "service_i7ceuoc",
             "template_84j0i1j",
             e.target,
             "user_zdoPizlRZAwhMcwZtcXI5"
-        ).then(res => { console.log(res) }).catch(error => console.log(error));
-        e.preventDefault();
+      ).then(res => { console.log(res) }).catch(error => console.log(error));
+      e.target.reset();
+      e.preventDefault();
+      setShowResult(true);
     }
     return (
       <div id="contact" className="h-auto bg-gray-800">
@@ -17,13 +28,40 @@ const Contact = () => {
           <h1 className="text-2xl text-white mb-10">
             Let's Create <br /> Something Big
           </h1>
+          {result ? showResult() : null}
           <div>
             <form className="flex flex-col mx-auto " onSubmit={sendEmail}>
-              <input className="outline-none p-2 mb-1 rounded-sm" type="text" name="fullName" placeholder="Full Name" />
-              <input className="outline-none p-2 mb-1 rounded-sm"  type="email" name="email" placeholder="Email Adress" />
-              <input className="outline-none p-2 mb-1 rounded-sm"  type="text" name="mobile" placeholder="Phone Number"/>
-              <textarea className="outline-none p-2  mb-2 rounded-sm"  rows="10" cols="20" placeholder="Message" name="message" />
-              <button type="submit" className="bg-red-50 text-blue-500 md:w-1/6 w-auto mb-7 py-2 px-3 rounded-sm text-blod"><span >Send A Message</span></button>
+              <input
+                className="outline-none p-2 mb-1 rounded-sm"
+                type="text"
+                name="fullName"
+                placeholder="Full Name"
+              />
+              <input
+                className="outline-none p-2 mb-1 rounded-sm"
+                type="email"
+                name="email"
+                placeholder="Email Adress"
+              />
+              <input
+                className="outline-none p-2 mb-1 rounded-sm"
+                type="text"
+                name="mobile"
+                placeholder="Phone Number"
+              />
+              <textarea
+                className="outline-none p-2  mb-2 rounded-sm"
+                rows="10"
+                cols="20"
+                placeholder="Message"
+                name="message"
+              />
+              <button
+                type="submit"
+                className="bg-red-50 text-blue-500 md:w-1/6 w-auto mb-7 py-2 px-3 rounded-sm text-blod"
+              >
+                <span>Send A Message</span>
+              </button>
             </form>
           </div>
         </div>
